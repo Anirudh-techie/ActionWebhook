6b9b6ff01f4b8a85919a4ba993568ffdc75f865c
google-action <admin@gmail.com>
2021-04-12 14:39:09 +0530

    fgh

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const bodyParser = require('body-parser');  
+const bodyParser = require('body-parser');
 
 // Import the service function and various response classes
 const { conversation } = require('@assistant/conversation');
@@ -8,17 +8,30 @@ const { conversation } = require('@assistant/conversation');
 const app = conversation();
 
 app.handle('getGem', conv => {
-var BirthDate= conv.intent.params.BirthDate.resolved;
-var prev = 0;
-BirthDate.toString().split("").forEach((n)=>{
-  n = parseInt(n)
-  prev +=n;
-});
-conv.add(prev.toString())
-});
+  var BirthDate = conv.intent.params.BirthDate.resolved;
+  var prev = 0;
+  BirthDate.toString().split("").forEach((n) => {
+    n = parseInt(n)
+    prev += n;
 
+  });
+
+  conv.add(numToGem(prev))
+
+  
+});
 
 var exApp = express();
 
 exApp.use(bodyParser.json(), app).listen(process.env.PORT);
 
+function numToGem(num){
+  
+  switch (num) {
+    case 1:
+      return 'Ruby'
+    
+    default:
+      return null;
+  }
+}
