00e6b1aa6555327ef91506689cf4d6f049bff403
google-action <admin@gmail.com>
2021-04-12 15:28:21 +0530

    5454545545

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.handle('getGem', conv => {
       prev += n;
       if(prev.toString().split("").length>1){
         num = prev;
+        console.log(num,root(num))
         return root(num);
       }
     });
